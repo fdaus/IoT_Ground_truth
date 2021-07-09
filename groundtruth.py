a97d0fc62524b95a10615ac2739e05394f13d83b
Firdaus Afifi <fdaus.isa@gmail.com>
2021-07-10 00:36:20 +0800

    update pyshark

--- groundtruth.py
+++ groundtruth.py
@@ -1,4 +1,14 @@
 import pyshark
-
+# import asyncio
+# asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+# asyncio.run(main())
 stream = pyshark.FileCapture("16-09-25.pcap")
-print(stream[0])
+#print (stream[0])
+print(dir(stream))
+def get_features(packet):
+    header_f = {'src_ip' : str(packet.ip.src), 
+        'dst_ip' : str(packet.ip.dst),  
+        'frozen' : frozenset((str(packet.ip.src), str(packet.ip.dst))),
+        'payload' : int(packet.tcp.len)}
+    
+    process_row(header_f, 'capture')
