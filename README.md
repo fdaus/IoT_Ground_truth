94d4b8b69f78641f0fea02da6c9cdc36c6a2c438
fdaus <fdaus.isa@gmail.com>
2021-10-13 00:07:50 +0800

    update code

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 
+1- split pcap by unique mac address - save in different folder
+2- extract features into csv
+
 Load pcap file
 search for unique mac address
-define gateway
 input label for unique mac address
-opt:  1- split pcap by unique mac address - save in diff folder
-      2- save to df
--- mycode.py
+++ mycode.py
@@ -6,22 +6,62 @@
 
     This program is an implementation of groundtruth https://github.com/fdaus/IoT_Ground_truth
     
-    Device isolated, it takes input pcaps and split each packets according to its mac address.
+    slicing traffic, it takes input pcaps and split packets according to its source/destination mac address.
 
-Usage: device_isolation.py <inputdir> <outputdir>
+Usage: mycode.py <inputdir> <outputdir>
 
-Example:python device_isolation.py data pcap-isolated
+Example:python mycode.py data pcap-isolated
 '''
 #device_isolation.py -d <inputdir> [or] -i <inputpcap> -l <label> [and] -o <outputdir>
+
 import subprocess
 
 import os
 
-#files = os.listdir("data")
+DUMP="data_portion"
+SLICE="pcap-sliced"
 
-#cmd="echo {}".format(files)
-#subprocess.run(cmd)
-#os.system(cmd)
+def ret_pcaps():
+    files = os.listdir(DUMP)
+    pcap_files = []
+    for file in files:
+        if '.pcap' in file or '.pcapng' in file:
+            pcap_files.append(DUMP+'/'+file)
+    return pcap_files
 
-exit_code = subprocess.call('tshark.sh',shell=True, input="2")
-print("done")
+def create_outputdir(outputdir,device_label):
+
+    dirpath=outputdir+'/'+device_label
+    if not os.path.isdir(dirpath):
+        os.makedirs(dirpath)
+
+def main():
+    allpcaps = ret_pcaps()
+    print(len(allpcaps)," pcap files")
+
+    device = pd.read_csv(DEVICE)
+    j=FILENUMBER
+    for pcap_file in allpcaps:
+        i=0
+        
+        print("Examining pcap file ",j)
+        print("start time ",get_time())
+        while i < device.shape[0]:
+            
+            print("\nfrom {}".format(pcap_file))
+            print("\nIsolating address "+device.iloc[i,1]+" for device "+device.iloc[i,0])
+            
+            create_outputdir(ISOLATED,device.iloc[i,0])
+
+            cmd ='tshark -r {} -Y "eth.addr eq {}" -w "{}/{}/{}.pcap"'.format(pcap_file,device.iloc[i,1],ISOLATED, device.iloc[i,0],j )
+            
+            os.system(cmd)
+            
+            print("\nComplete: {}".format(device.iloc[i,0]))
+            i+= 1
+        
+        j+=1
+        print("End time",get_time())
+        print("\n#############################################################")
+        
+    print("siap")
